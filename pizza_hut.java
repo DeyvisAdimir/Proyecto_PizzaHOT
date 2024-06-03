f628c400502d6491a0620b326b02a0b63cad6a96
jhonatanaguilarpaja <holagringopoli3216@gmail.com>
2024-06-03 17:00:15 -0500

    Add files via upload

--- pizza_hut.java
+++ pizza_hut.java
@@ -1,27 +1,38 @@
 import java.util.Scanner;
-
 public class pizza_hut {
-    public static double calcularCostoTotal(String bebida) {
+    public static double calcularCostoTotal(String ofertas) {
         double costoTotal = 0.0;
 
-        switch (bebida.toLowerCase()) {
-            case "coca cola sin azucar":
-                costoTotal = 4.90;
+        switch (ofertas) {
+            case "TRIPACK FAMILIAR":
+                costoTotal = 109.90;
                 break;
-            case "inka kola sin azucar":
-                costoTotal = 4.90;
+            case "CHEESE BITES":
+                costoTotal = 49.90;
                 break;
-            case "sprite":
-                costoTotal = 4.90;
+            case "BANQUETE FAMILIAR":
+                costoTotal = 49.90;
                 break;
-            case "fanta":
-                costoTotal = 4.90;
+            case "COMBO FIJA":
+                costoTotal = 24.90;
                 break;
-            case "agua san luis sin gas":
-                costoTotal = 4.90;
+            case "COMBO MEDIANO":
+                costoTotal = 19.90;
+                break;
+            case "HUT CHEESE MEDIANO":
+                costoTotal = 26.90;
+                break;
+            case "PIZZA GRANDE DE HUT CHEESE":
+                costoTotal = 32.90;
+                break;
+            case "BANQUETE GRANDE":
+                costoTotal = 39.90;
+                break;
+            case "BANQUETE GRANDE DE CHEESE":
+                costoTotal = 45.90;
                 break;
             default:
-                System.out.println("No disponible");
+                System.out.println("oferta no disponible");
                 costoTotal = 0.0;
                 break;
         }
@@ -30,23 +41,32 @@ public class pizza_hut {
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner nyx = new Scanner(System.in);
 
-        System.out.println("¡Bienvenido a Pizza Hut!");
-        System.out.println("Nuestras bebidas disponibles son:");
-        System.out.println("1. Coca Cola sin azucar");
-        System.out.println("2. Inka Kola sin azucar");
-        System.out.println("3. Sprite");
-        System.out.println("4. Fanta");
-        System.out.println("5. Agua San Luis sin gas");
+        System.out.println("BIENVENIDO A PIZZA HUT!");
+        System.out.println("ESTAS SON NUESTRAS OFERTAS");
+        System.out.println("NUESTROS  COMBOS  DISPONIBLES SON:");
+        System.out.println("1. TRIPACK FAMILIAR");
+        System.out.println("2. CHEESE BITES");
+        System.out.println("3. BANQUETE FAMILIAR");
+        System.out.println("4. COMBO MEDIANO");
+        System.out.println("5. COMBO FIJA");
+        System.out.println("6. HUT CHEESE MEDIANO");
+        System.out.println("7. PIZZA GRANDE DE HUT CHEESE");
+        System.out.println("8. BANQUETE GRANDE");
+        System.out.println("9. BANQUETE GRANDE DE CHEESE");
 
-        System.out.print("Ingrese el nombre de la bebida que desea ordenar: ");
-        String bebida = scanner.nextLine();
+        System.out.print("Ingrese el nombre del combo que desea ordenar: ");
+        String ofertas = nyx.nextLine();
 
-        double costoTotal = calcularCostoTotal(bebida);
+        double costoTotal = calcularCostoTotal(ofertas.toUpperCase());
 
         if (costoTotal > 0) {
             System.out.println("El costo total de su orden es: $" + costoTotal);
         }
+
+
+
     }
+
 }
